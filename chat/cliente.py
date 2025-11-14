ba672d46e13365ec17603e4813802470cc65441b
Joao <araujo.nascimento@escolar.ifrn.edu.br>
2025-11-14 20:50:16 -0300

    Finalização do chat com interface gráfica

--- chat/cliente.py
+++ chat/cliente.py
@@ -1,5 +1,42 @@
 import customtkinter
 from CTkMessagebox import CTkMessagebox
+import socket
+import threading
+
+def enviar_mensagem():
+    global cliente
+    global nome
+
+    mensagem = campo_mensagem.get().strip()
+    if not mensagem:
+        return
+    
+    mensagem = f"[{nome}] {mensagem}"
+    campo_mensagem.delete(0, "end")
+
+    try:
+        cliente.send(mensagem.encode())
+        memo_mensagens.configure(state = "normal")
+        memo_mensagens.insert("end", mensagem + "\n")
+        memo_mensagens.see("end")
+        memo_mensagens.configure(state = "disabled")
+    except Exception as e:
+        CTkMessagebox(title="Erro", message="Erro no envio de mensagem", 
+                      icon="warning")
+
+
+def receber_mensagem():
+    global cliente 
+
+    while True:
+        try:
+            mensagem = cliente.recv(10000).decode()
+            memo_mensagens.configure(state = "normal")
+            memo_mensagens.insert("end", mensagem + "\n")
+            memo_mensagens.see("end")
+            memo_mensagens.configure(state = "disabled")
+        except Exception as e:
+            pass
 
 def conectar():
     global nome
@@ -10,6 +47,25 @@ def conectar():
         CTkMessagebox(title="Erro", message="Antes, digite seu nome", 
                       icon="warning")
         return    
+    
+    nome = nome_valor
+
+    try:
+        cliente = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        cliente.connect(("192.168.56.1", 5000))
+
+        campo_mensagem.configure(state = "normal")
+        botao_enviar.configure(state = "normal")
+        botao_conectar.configure(state = "disabled")
+        campo_nome.configure(state = "disabled")
+
+        thread = threading.Thread(target=receber_mensagem)
+        thread.start()
+
+    except Exception as e:
+        CTkMessagebox(title="Erro", message="Erro na conexão", 
+                      icon="warning")
+    
 
 
 customtkinter.set_appearance_mode("dark")
@@ -40,7 +96,7 @@ campo_mensagem = customtkinter.CTkEntry(frame, state="disabled")
 campo_mensagem.pack(side="left", fill="x", expand=True, padx=(0,10))
 
 botao_enviar = customtkinter.CTkButton(frame, text="Enviar", 
-                                       command=None, state="disabled")
+                                       command=enviar_mensagem, state="disabled")
 botao_enviar.pack(side="right")
 
 cliente = None
